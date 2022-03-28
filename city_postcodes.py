bfbbb8baec50bd806a49373711a5031a36489b12
davetc4k <93985333+davetc4k@users.noreply.github.com>
2022-03-28 12:57:01 +0100

    script to scrape city postcode data from wikipedia
    Wikipedia have publicly accessible data for cites and their postcodes . This script parses that data

---
+++ city_postcodes.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import csv
+
+url = (
+    r"https://en.wikipedia.org/wiki/List_of_postcode_districts_in_the_United_Kingdom#PA"
+)
+tables = pd.read_html(url)  # Returns list of all tables on page
+table = tables[1]
+
+with open("city_postcodes.csv", "w") as file:
+    writer = csv.writer(file)
+    header = ["index", "area", "sector", "city", ""]
+    writer.writerow(header)
+    for index, row in table.iterrows():
+
+        writer.writerow(row)
